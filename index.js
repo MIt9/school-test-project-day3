0da1d92d633d12fec94fc24df3e8d1c2fbd2e99e
siniidrozd@gmail.com <K@ns@s2010>
2015-03-20 05:56:07 +0200

    change user key chang users controler

--- index.js
+++ index.js
@@ -3,28 +3,17 @@
 var http = require("http"),
     url = require("url"),
     routes = require("./routes");
- //   getjson = require("getjsoneasily"),
- //   uc = require("./lib/usersController");
-
-
-//var User = require('./lib/users'),
-//    user = [];
-
-
-
 
 function requestHandler(request, response){
 
     var parsedUrl = url.parse(request.url, true),
         method = request.method.toLowerCase();
 
-
-
-
     if(routes[method][parsedUrl.pathname]){
         routes[method][parsedUrl.pathname](request, response, function(err){
             if(err){
                 response.statusCode = 500;
+
                 response.write(JSON.stringify({
                     "message": err.message,
                     "status": "Internal Server Error"
@@ -32,21 +21,11 @@ function requestHandler(request, response){
                 response.end();
             }else{
                 response.statusCode = 404;
-                response.end();
+                response.end("Wrong string in url");
             }
         });
     }else{
-
-        //console.log("statusCode ==>" + response.statusCode);
         response.statusCode = 404;
-      //  uc.methodCheck(method);
-       /*
-        getjson(url, function(data){
-            // Do something with data
-            console.log(data);
-        });
-        */
-
         response.end("Not Found bla");
     }
 }
